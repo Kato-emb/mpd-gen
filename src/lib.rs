f1a9b861cf53979956828c4094b0be8c8cb9e697
Ryouhei Kato <r-kato@musen.co.jp>
2024-10-14 20:28:57 +0900

    公開範囲を修正

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,16 @@ mod types;
 #[macro_use]
 mod macros;
 
-pub use element::segment::{Segment, SegmentBuilder, SegmentTimeline, SegmentTimelineBuilder};
-use error::MpdError;
+pub use element::adapt::*;
+pub use element::mpd::*;
+pub use element::period::*;
+pub use element::repr::*;
+pub use element::segment::*;
+pub use element::*;
+
+pub use definition::*;
+pub use types::*;
+
+pub use error::MpdError;
 
 pub type Result<T> = std::result::Result<T, MpdError>;
